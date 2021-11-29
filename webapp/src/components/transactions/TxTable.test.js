42e5b1f87d2b1b031a69fbcadac185b819ba4104
Matthew Lemon <mlemon@instructure.com>
2021-11-29 15:19:24 -0700

    add specs for TxTable

--- webapp/src/components/transactions/TxTable.test.js
+++ webapp/src/components/transactions/TxTable.test.js
@@ -1,8 +1,93 @@
-import { render } from '@testing-library/react'
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import { SettingsManagerContext } from '../SettingsManager'
+import { TxTable } from './TxTable'
+
+const makeProps = overrides => ({
+  data: [
+    {
+      id: 'transaction_1',
+      user_id: 'user_1',
+      description: 'description_1',
+      merchant_id: 'merchant_1',
+      debit: true,
+      credit: false,
+      amount: 123.05
+    },
+    {
+      id: 'transaction_2',
+      user_id: 'user_2',
+      description: 'description_2',
+      merchant_id: 'merchant_2',
+      debit: true,
+      credit: false,
+      amount: 1337.13
+    },
+    {
+      id: 'transaction_3',
+      user_id: 'user_3',
+      description: 'description_3',
+      merchant_id: 'merchant_3',
+      debit: false,
+      credit: true,
+      amount: 45.65
+    }
+  ],
+  onDelete: jest.fn(),
+  onEdit: jest.fn(),
+  ...overrides
+})
+
+const setup = (props, romanize) => {
+  return render(
+    <SettingsManagerContext.Provider value={{ romanize }}>
+      <TxTable {...props} />
+    </SettingsManagerContext.Provider>
+  )
+}
 
 describe('Transactions Table', () => {
-  it('should show user "employee4" with amount "150"', () => {
-
+  it('displays transactions', () => {
+    const props = makeProps()
+    const container = setup(props)
+    props.data.forEach(transaction => {
+      expect(container.getByText(transaction.id)).toBeInTheDocument()
+      expect(container.getByText(transaction.user_id)).toBeInTheDocument()
+      expect(container.getByText(transaction.description)).toBeInTheDocument()
+      expect(container.getByText(transaction.merchant_id)).toBeInTheDocument()
+      expect(container.getByText(transaction.amount.toString())).toBeInTheDocument()
+    })
   })
 
+  it('calls the delete callback', () => {
+    const props = makeProps()
+    const container = setup(props)
+
+    const deleteButton = container.getAllByTestId('DeleteIcon')[0]
+    fireEvent.click(deleteButton)
+    expect(props.onDelete).toHaveBeenCalledWith({ variables: { id: props.data[0].id } })
+  })
+
+  it('calls the edit callback', () => {
+    const props = makeProps()
+    const container = setup(props)
+
+    const editButton = container.getAllByTestId('EditIcon')[0]
+    fireEvent.click(editButton)
+    expect(props.onEdit).toHaveBeenCalledWith(props.data[0])
+  })
+
+  it('converts the transaction amounts to roman numerals when the setting is enabled', () => {
+    const props = makeProps()
+    const container = setup(props, true)
+
+    // 123
+    expect(container.getByText('CXXIII')).toBeInTheDocument()
+
+    // 1337
+    expect(container.getByText('MCCCXXXVII')).toBeInTheDocument()
+
+    // 46, because we round to the nearest whole number before romanizing a number
+    expect(container.getByText('XXXXVI')).toBeInTheDocument()
+  })
 })
--- webapp/src/utils/index.js
+++ webapp/src/utils/index.js
@@ -1,26 +1,21 @@
 export const romanizeNumber = number => {
   // Since there is no clear answer on how romans dealt with
-  // fractions, I'm just going to split them on the decimal
-  // and convert them to roman numerals and then combine them
-  // again with a decimal
-  const parts = number.toString().split('.')
-  if (parts.length === 2) {
-    return romanizeNumber(parseInt(parts[0])) + '.' + romanizeNumber(parseInt(parts[1]))
-  } else {
-    const mCount = Math.floor(number / 1000)
-    number -= mCount * 1000
-    const dCount = Math.floor(number / 500)
-    number -= dCount * 500
-    const cCount = Math.floor(number / 100)
-    number -= cCount * 100
-    const lCount = Math.floor(number / 50)
-    number -= lCount * 50
-    const xCount = Math.floor(number / 10)
-    number -= xCount * 10
-    const vCount = Math.floor(number / 5)
-    number -= vCount * 5
-    const iCount = number
+  // fractions, I'm just going to rount the number to the
+  // nearest whole number and then convert it
+  number = Math.round(number)
+  const mCount = Math.floor(number / 1000)
+  number -= mCount * 1000
+  const dCount = Math.floor(number / 500)
+  number -= dCount * 500
+  const cCount = Math.floor(number / 100)
+  number -= cCount * 100
+  const lCount = Math.floor(number / 50)
+  number -= lCount * 50
+  const xCount = Math.floor(number / 10)
+  number -= xCount * 10
+  const vCount = Math.floor(number / 5)
+  number -= vCount * 5
+  const iCount = number
 
-    return 'M'.repeat(mCount) + 'D'.repeat(dCount) + 'C'.repeat(cCount) + 'L'.repeat(lCount) + 'X'.repeat(xCount) + 'V'.repeat(vCount) + 'I'.repeat(iCount)
-  }
+  return 'M'.repeat(mCount) + 'D'.repeat(dCount) + 'C'.repeat(cCount) + 'L'.repeat(lCount) + 'X'.repeat(xCount) + 'V'.repeat(vCount) + 'I'.repeat(iCount)
 }
